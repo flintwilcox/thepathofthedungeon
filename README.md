e3f0399f826f02d62b43ff29d4c2d6d92107a170
flintwilcox <flintwilcox@gmail.com>
2026-02-11 20:23:18 -0500

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://flintwilcox.github.io/thepathofthedungeon/](https://flintwilcox.github.io/thepathofthedungeon/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/flintwilcox/thepathofthedungeon** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/flintwilcox/thepathofthedungeon** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ tilemap.g.ts
@@ -0,0 +1,121 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "room1":
+            case "roomStart1":return tiles.createTilemap(hex`19000f00040404040404040404040404040404040404040404040404040404040404040404040404040404040404040104010401040404050505050505050505050505050505050505050505050504040506050505050505070505050505050505050505050305040405050707050505050705050505050505050505050505050404050507070505050505070505070707070707070707070504040505050505050705050705050705050505050505050505040405050505050507050507050507050505050505050505050404050505050707070505050505070505070705050505050504040505050507050505050707070705050707050505050505040405050505070505050505050505050507070505050505050404050505050705050805050505050505070705050505050504040505050507050205050505050505050707050505050505040405050505050505050505050505050505050505050505050404040404040404040404040404040404040404040404040404`, img`
+2222222222222222222222222
+222222222222222222.2.2.22
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2.......................2
+2222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.doorClosedNorth,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorDarkDiamond,sprites.dungeon.floorDark2,sprites.dungeon.floorLightMoss,sprites.dungeon.floorLight1,sprites.dungeon.hazardLava0,sprites.dungeon.floorMixed], TileScale.Sixteen);
+            case "room4":
+            case "room3":return tiles.createTilemap(hex`0f000f00040404040404040404040404040404040404040104040104040104040404040303030303030303030303030304040305030306060606040606060304040303030606030307070707070704040303060707030607070707070704040406060707060607070707070704040606060707030306060606030704040606060707060303060406060704040306060707070707070706060704040306060707070707070706040704040306040707060606080606060704040306030707030203060606060704040303030707030303070707070704040404040404040404040404040404`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 . 2 2 . 2 2 . 2 2 2 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . 2 2 2 2 2 2 2 
+2 . . . 2 2 . . 2 2 2 2 2 2 2 
+2 . . . 2 2 . . 2 2 2 2 2 2 2 
+2 . . . 2 2 . . . . . . . 2 2 
+2 . . . 2 2 . . . . . . . 2 2 
+2 . . . 2 2 2 2 2 2 2 . . 2 2 
+2 . . . 2 2 2 2 2 2 2 . . 2 2 
+2 . . . 2 2 . . . . . . . 2 2 
+2 . . . 2 2 . . . . . . . 2 2 
+2 . . . 2 2 . . . 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.doorClosedNorth,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLight0,sprites.dungeon.floorDark4,sprites.dungeon.floorDarkDiamond,sprites.dungeon.floorDark0,sprites.dungeon.darkGroundSouthEast1,sprites.dungeon.floorMixed], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10001100020d020d020d0d020d0d020d020d0202030202020b02020b02020b0202020206080a0a01010101010101010101010104080a0a01010101010101010101010104080a010101010f01010f0f01010101040801010101010101010101010101010408010a0101010101011101010101010408010101010101010101010101010104080110010101010101010101010a01040801010101010101010f0101010a010408010101010101010f0f01010101010408010a010e01010f01011001010101040801010101010101010101010a0a010408010a0a01010101010101010a0a01040801010a010c01010a01010a0a0101040801010101010101010101010101010407090909090909090909090909090905`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 . 2 2 . 2 2 . 2 2 2 2 2 
+2 2 2 . . . . . . . . . . . . 2 
+2 2 2 . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . 2 . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . 2 . 2 
+2 . . . . . . . . . . . . 2 . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . 2 . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . 2 2 . 2 
+2 . 2 2 . . . . . . . . 2 2 . 2 
+2 . . 2 . . . . 2 . . 2 2 . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorDark0,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.stairLadder,sprites.dungeon.doorClosedNorth,sprites.dungeon.collectibleInsignia,sprites.dungeon.greenOuterNorth2,sprites.dungeon.floorMixed,sprites.dungeon.floorDark4,sprites.dungeon.floorDark3,sprites.dungeon.floorDarkDiamond], TileScale.Sixteen);
+            case "room0":
+            case "room2":return tiles.createTilemap(hex`1e0014000404040404040404040404040404040404040404040404040404040404040404040401040406040401040404040401040404040404040404040404040403030303030303030303030303030303030303030303030303030303040403030303030303030303030303030303030303030303030d0d0d0a030404030303070303030303030303030303030c030303030303030d0d03030404030303030303030303030303030303030c0c0303030307030d0d0303040403030303030303030d030303030303030b0c0c0303030303030303030404030a0303030303030303090903030303030b0c03030303030303030304040303030d030303030305090303030303030b0c03030303030303030304040303030d0d0303030303090303030303030b0b03030303080803030304040303030d0d030d030303090303070303030b0b03030303080303030304040303030309090d030303090308030303030b0b030303030303070303040403030303030309030303030303080303030c0c0303030303030303030404030303030b0c0b0c0303030303030303030b0c0303030703030303030404030303030303030c0c0b0303030303030303030303030e03030303030404030703030303030303030303030b0c0c03030909030303030209030304040303070d0d03030303030303030303030303030909030303030903030404030303030d0d0303030307030303030a03030303030303030309030304040303030303030303030303030303030303030303030303030309030304040404040404040404040404040404040404040404040404040404040404`, img`
+222222222222222222222222222222
+2222.22222.22222.2222222222222
+2............................2
+2.......................222..2
+2........................22..2
+2........................22..2
+2........2...................2
+2............................2
+2...2........................2
+2...22.......................2
+2...22.2.....................2
+2......2.....................2
+2............................2
+2............................2
+2............................2
+2............................2
+2...22.......................2
+2....22......................2
+2............................2
+222222222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.doorClosedNorth,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLight0,sprites.dungeon.floorDark4,sprites.dungeon.floorDarkDiamond,sprites.dungeon.floorDark0,sprites.dungeon.floorLight4,sprites.dungeon.floorLight5,sprites.dungeon.floorLightMoss,sprites.dungeon.floorLight3,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.dungeon.stairLadder,sprites.dungeon.floorMixed], TileScale.Sixteen);
+            case "roomStart":
+            case "level2":return tiles.createTilemap(hex`0f000f00080808080808080808080808080808060606070107070107070107060606020303030503030503030503030302020304030505050505050503030302020303030303030503030303030302020303030303040503030304030302020303030303030503030303030302020303030303030503030303030302020303040303030504030304030302020303030303040503030303030302020303030303030503030303030302020303030303030503030304030302020303040303030903030303030302020303030303030303030303030302020202020202020202020202020202`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 . 2 2 . 2 2 . 2 2 2 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.doorClosedNorth,sprites.builtin.forestTiles0,sprites.castle.tileGrass3,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth0,sprites.dungeon.collectibleInsignia], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
